9c076fa30c339568e9c15749d1b91fbce9a7f884
olayinka <adeleyeolayinka73@gmail.com>
2024-10-07 18:28:16 +0100

    add file editor layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,27 +15,53 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { cn } from '@/lib/utils'
+
+
 
 const Sidebar = () => {
+
 
     return (
         <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  p-6 pt-28 text-black max-sm:hidden lg:w-[180px]">
             <div className="flex flex-1 flex-col gap-6 ">
+
+
+
+
+
                 {sidebarLinks.map((link) => {
+                    const LikedLink = link.label === 'like'
 
                     return (
                         <div key={link.label}>
-                            <Link
-                                href={link.route}
-                                className='flex gap-4 items-center p-4 rounded-lg justify-end'
-                            >
-                                <Image
-                                    src={link.imgUrl}
-                                    alt={link.label}
-                                    width={24}
-                                    height={24}
-                                />
-                            </Link>
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+
+                                        <Link
+                                            href={link.route}
+                                            className='flex gap-4 items-center p-4 rounded-lg justify-end'
+                                        >
+                                            <Image
+                                                src={link.imgUrl}
+                                                alt={link.label}
+                                                width={24}
+                                                height={24}
+                                            />
+                                        </Link>
+                                    </TooltipTrigger>
+                                    <TooltipContent className='bg-black text-white'>
+                                        {link.tooltip}
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
                         </div>
 
                     )
@@ -56,7 +82,7 @@ const Sidebar = () => {
                                 src="/icons/file.svg"
                                 width={22}
                                 height={22}
-                                alt="Avatar"
+                                alt="file"
                             />
                         </div>
                         <DropdownMenuSeparator />
--- constants/index.ts
+++ constants/index.ts
@@ -3,16 +3,19 @@ export const sidebarLinks = [
         label: 'Like',
         route: '/',
         imgUrl: '/icons/like.svg',
+        tooltip: "Add reaction"
     },
     {
         label: 'comment',
         route: '/',
         imgUrl: '/icons/comment.svg',
+        tooltip: "Jump to comment"
     },
     {
         label: 'save',
         route: '/',
         imgUrl: '/icons/save.svg',
+        tooltip: "Save"
     },
 ]
 
