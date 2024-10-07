5aeb510b2769786d67cf6117c1d816cea7ba444f
olayinka <adeleyeolayinka73@gmail.com>
2024-10-07 17:25:49 +0100

    add file editor layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import { sidebarLinks, sidebarMediaLinks } from '@/constants'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import Image from 'next/image'
 
 
@@ -18,13 +17,11 @@ import {
 
 
 const Sidebar = () => {
-    const pathname = usePathname()
 
     return (
         <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  p-6 pt-28 text-black max-sm:hidden lg:w-[180px]">
             <div className="flex flex-1 flex-col gap-6 ">
                 {sidebarLinks.map((link) => {
-                    const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`)
 
                     return (
                         <div>
